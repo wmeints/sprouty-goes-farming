17439d905ef442cf2d966f473f9552a0afeb5d6b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-08-03 21:14:08 +0200

    Add AI so the cows are curious about the bunny when it gets close

--- Assets/Scripts/CowController.cs
+++ Assets/Scripts/CowController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CowController : MonoBehaviour
 {
     public Collider2D parentCollider;
     public Collider2D childCollider;
+    public NavMeshAgent agent;
+    public Transform player;
     
     // Start is called before the first frame update
     void Start()
@@ -13,11 +17,23 @@ public class CowController : MonoBehaviour
         Physics2D.IgnoreCollision(
             childCollider, 
             parentCollider, true);
+
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var distance = player.transform.position - transform.position;
+
+        if (distance.magnitude < 5f)
+        {
+            agent.destination = player.transform.position;
+            agent.enabled = true;
+        }
+        else
+        {
+            agent.enabled = false;
+        }
     }
 }
