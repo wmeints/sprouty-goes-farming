3f62c4e528d529c4bc0ea9fa23518f76414220ae
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-08-03 21:39:55 +0200

    Add animations for eating and moving

--- Assets/Scripts/CowController.cs
+++ Assets/Scripts/CowController.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = System.Random;
 
 public class CowController : MonoBehaviour
 {
@@ -10,30 +9,57 @@ public class CowController : MonoBehaviour
     public Collider2D childCollider;
     public NavMeshAgent agent;
     public Transform player;
+    public Animator animator;
+
+    private DateTime _eatingTimestamp;
+    private Random _random;
     
     // Start is called before the first frame update
     void Start()
     {
+        _random = new Random();
+        _eatingTimestamp = DateTime.UtcNow;
+        
         Physics2D.IgnoreCollision(
             childCollider, 
             parentCollider, true);
 
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
     void Update()
     {
         var distance = player.transform.position - transform.position;
 
+        // Cows don't eat while they are running.
+        if (agent.velocity.sqrMagnitude > 0.01)
+        {
+            animator.SetBool("Eating", false);
+        }
+        
+        // Cows are curious animals. They'll follow you around when you're getting close.
         if (distance.magnitude < 5f)
         {
-            agent.destination = player.transform.position;
             agent.enabled = true;
+            agent.destination = player.transform.position;
         }
         else
         {
             agent.enabled = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        var timeDifference = DateTime.UtcNow - _eatingTimestamp;
+
+        if (timeDifference.TotalSeconds > 3 && agent.velocity.sqrMagnitude < 0.01)
+        {
+            _eatingTimestamp = DateTime.UtcNow;
+            animator.SetBool("Eating", _random.NextDouble() < 0.7);
         }
     }
 }
